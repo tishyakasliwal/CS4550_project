cce03fbbde614a0396cf91dbd4278b51c86bbde2
Tishya Kasliwal <tishyakasliwal@gmail.com>
2025-02-04 23:28:15 -0500

    ch 2.2 done

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,4 +1,18 @@
 import "./index.css";
+import BackgroundColors from "./BackgroundColors.tsx";
+import ForegroundColors from "./ForegroundColors.tsx";
+import Borders from "./Borders.tsx";
+import Margins from "./Margins.tsx";
+import Padding from "./Padding.tsx";
+import Corners from "./Corners.tsx";
+import Dimensions from "./Dimensions.tsx";
+import Positions from "./Positions.tsx";
+import Zindex from "./Zindex.tsx";
+import Float from "./Float.tsx";
+import GridLayout from "./GridLayout.tsx";
+import Flex from "./Flex.tsx";
+import ReactIconsSampler from "./ReactIcons.tsx";
+
 export default function Lab2() {
   return (
     <div id="wd-lab2">
@@ -32,5 +46,42 @@ Here's another paragraph using a different ID and a different look and
         </h4>
 
       </div>
+      <div id="wd-css-document-structure">
+        <div className="wd-selector-1">
+          <h3>Document structure selectors</h3>
+          <div className="wd-selector-2">
+            Selectors can be combined to refer elements in particular
+            places in the document
+            <p className="wd-selector-3">
+              This paragraph's red background is referenced as
+              <br />
+              .selector-2 .selector3<br />
+              meaning the descendant of some ancestor.<br />
+              <span className="wd-selector-4">
+                Whereas this span is a direct child of its parent
+              </span><br />
+                You can combine these relationships to create specific 
+                styles depending on the document structure
+            </p>
+          </div>
+        </div>
+        <ForegroundColors />
+        <BackgroundColors />
+        <Borders />
+        <Padding />
+        <Margins />
+        <Corners />
+        <Dimensions />
+        <Positions />
+        <Zindex />
+        <Float />
+        <GridLayout />
+        <Flex />
+        <ReactIconsSampler />
+
+
+        
+      </div>
+
 
     </div>);}
