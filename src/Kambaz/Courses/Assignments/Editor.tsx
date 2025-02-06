5078dd9efef43a54d6b2ca99ff08259447cfe715
Tishya Kasliwal <tishyakasliwal@gmail.com>
2025-02-06 00:16:43 -0500

    done

--- src/Kambaz/Courses/Assignments/Editor.tsx
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -1,111 +1,105 @@
+
+
+import { Form, Button, Row, Col } from "react-bootstrap";
+
 export default function AssignmentEditor() {
-    return (
-      <div id="wd-assignments-editor">
-        <label htmlFor="wd-name">Assignment Name</label>
-        <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
-        <textarea id="wd-description">
-          The assignment is available online Submit a link to the landing page of
-        </textarea>
-        <br />
-        <table>
-          <tr>
-            <td align="right" valign="top">
-              <label htmlFor="wd-points">Points</label>
-            </td>
-            <td>
-              <input id="wd-points" value={100} />
-            </td>
-          </tr> <br></br>
-          <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-assignment-group">Assignment Group</label>
-          </td>
-          <td>
-            <select id="wd-assignment-group">
+  return (
+    <div id="wd-assignments-editor" className="p-3">
+      <Form>
+        <Form.Group className="mb-3" controlId="wd-name">
+          <Form.Label>Assignment Name</Form.Label>
+          <Form.Control type="text" defaultValue="A1 - ENV + HTML" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="wd-description">
+          <Form.Label>Description</Form.Label>
+          <Form.Control as="textarea" rows={3} defaultValue="The assignment is available online. Submit a link to the landing page of" />
+        </Form.Group>
+
+        <Row className="mb-3">
+          <Form.Label column sm={2} htmlFor="wd-points">Points</Form.Label>
+          <Col sm={10}>
+            <Form.Control type="number" id="wd-points" defaultValue={100} />
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Label column sm={2} htmlFor="wd-assignment-group">Assignment Group</Form.Label>
+          <Col sm={10}>
+            <Form.Select id="wd-assignment-group">
               <option value="ASSIGNMENTS">ASSIGNMENTS</option>
-            </select>
-          </td>
-        </tr> <br></br>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-display-grade">Display Grade</label>
-          </td>
-          <td>
-            <select id="wd-display-grade">
+            </Form.Select>
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Label column sm={2} htmlFor="wd-display-grade">Display Grade</Form.Label>
+          <Col sm={10}>
+            <Form.Select id="wd-display-grade">
               <option value="percentage">Percentage</option>
               <option value="points">Points</option>
               <option value="letter">Letter Grade</option>
               <option value="gpa">GPA</option>
               <option value="pass_fail">Pass/Fail</option>
-            </select>
-          </td>
-        </tr> <br></br>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-submission-type">Submission Type</label>
-          </td>
-          <td>
-            <select id="wd-submission-type">
+            </Form.Select>
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Label column sm={2} htmlFor="wd-submission-type">Submission Type</Form.Label>
+          <Col sm={10}>
+            <Form.Select id="wd-submission-type">
               <option value="online">Online</option>
               <option value="paper">Paper</option>
               <option value="external_tool">External Tool</option>
-            </select>
-          </td>
-        </tr>  <br></br>  
-        <tr>
-          <td align="right" valign="top">
-            <label>Online Entry Options</label>
-          </td>
-          <td>
-            <input type="checkbox" id="text-entry" name="online-entry" value="text-entry" />
-            <label htmlFor="text-entry">Text Entry</label><br />
-            <input type="checkbox" id="website-url" name="online-entry" value="website-url" />
-            <label htmlFor="website-url">Website URL</label><br />
-            <input type="checkbox" id="media-recordings" name="online-entry" value="media-recordings" />
-            <label htmlFor="media-recordings">Media Recordings</label><br />
-            <input type="checkbox" id="student-annotation" name="online-entry" value="student-annotation" />
-            <label htmlFor="student-annotation">Student Annotation</label><br />
-            <input type="checkbox" id="file-uploads" name="online-entry" value="file-uploads" />
-            <label htmlFor="file-uploads">File Uploads</label>
-          </td>
-        </tr> <br></br>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-assign-to">Assign To</label>
-          </td>
-          <td>
-            <input id="wd-assign-to" type="text" />
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-due-date">Due</label>
-          </td>
-          <td>
-            <input id="wd-due-date" type="date" />
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-available-from">Available From</label>
-          </td>
-          <td>
-            <input id="wd-available-from" type="date" />
-          </td>
-        </tr>
-        <tr>
-          <td align="right" valign="top">
-            <label htmlFor="wd-available-until">Available Until</label>
-          </td>
-          <td>
-            <input id="wd-available-until" type="date" />
-          </td>
-        </tr>
-        </table>
-        <div className="wd-buttons">
-        <button>Cancel</button>
-        <button>Save</button>
-      </div>       
-      </div>
-  );}
-  
+            </Form.Select>
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Label column sm={2}>Online Entry Options</Form.Label>
+          <Col sm={10}>
+            <Form.Check type="checkbox" id="text-entry" label="Text Entry" />
+            <Form.Check type="checkbox" id="website-url" label="Website URL" />
+            <Form.Check type="checkbox" id="media-recordings" label="Media Recordings" />
+            <Form.Check type="checkbox" id="student-annotation" label="Student Annotation" />
+            <Form.Check type="checkbox" id="file-uploads" label="File Uploads" />
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Label column sm={2} htmlFor="wd-assign-to">Assign To</Form.Label>
+          <Col sm={10}>
+            <Form.Control type="text" id="wd-assign-to" />
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Label column sm={2} htmlFor="wd-due-date">Due</Form.Label>
+          <Col sm={10}>
+            <Form.Control type="date" id="wd-due-date" />
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Label column sm={2} htmlFor="wd-available-from">Available From</Form.Label>
+          <Col sm={10}>
+            <Form.Control type="date" id="wd-available-from" />
+          </Col>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Label column sm={2} htmlFor="wd-available-until">Available Until</Form.Label>
+          <Col sm={10}>
+            <Form.Control type="date" id="wd-available-until" />
+          </Col>
+        </Row>
+
+        <div className="d-flex justify-content-end">
+          <Button variant="secondary" className="me-2">Cancel</Button>
+          <Button variant="primary">Save</Button>
+        </div>
+      </Form>
+    </div>
+  );
+}
--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -3,6 +3,8 @@ import AssignmentControls from "./AssignmentsControls";
 import { BsGripVertical } from "react-icons/bs";
 import AssignmentsBannerButtons from "./AssignmentsBannerButtons";
 import AssignmentsControlButtons from "./AssignmentsControlButtons";
+import { LuNotepadText } from "react-icons/lu";
+
 export default function Assignments() {
     return (
       <div id="wd-assignments">
@@ -15,31 +17,31 @@ export default function Assignments() {
 
     
         
-        <ul id="wd-assignment-list">
-          <li className="wd-assignment-list-item">
+        <ListGroup id="wd-assignment-list" className="wd-lessons rounded-0">
+          <ListGroup.Item className="wd-assignment-list-item">
             <a href="#/Kambaz/Courses/1234/Assignments/123"
                className="wd-assignment-link" >
-              A1 - ENV + HTML <AssignmentsControlButtons />
+              <BsGripVertical className="me-2 fs-3" /> <LuNotepadText className="me-2 fs-3"/>A1 - ENV + HTML <AssignmentsControlButtons />
             </a> 
             <p> Multiple Modules | <b>Not Available </b> until May 6 at 12:00 am |
-            <b> Due</b> May 13 at 11:59 pm | 100 pts </p></li>
-          <li className="wd-assignment-list-item">
+            <b> Due</b> May 13 at 11:59 pm | 100 pts </p></ListGroup.Item >
+          <ListGroup.Item className="wd-assignment-list-item">
             <a href="#/Kambaz/Courses/1234/Assignments/124"
                className="wd-assignment-link" >
-              A2 - CSS + BOOTSTRAP <AssignmentsControlButtons />
+              <BsGripVertical className="me-2 fs-3" /> <LuNotepadText className="me-2 fs-3"/> A2 - CSS + BOOTSTRAP <AssignmentsControlButtons />
             </a> <p> Multiple Modules | <b>Not Available </b> until May 13 at 12:00 am |
             <b> Due</b> May 20 at 11:59 pm | 100 pts </p>
 
-          </li>
-          <li className="wd-assignment-list-item">
+          </ListGroup.Item >
+          <ListGroup.Item className="wd-assignment-list-item">
             <a href="#/Kambaz/Courses/1234/Assignments/125"
                className="wd-assignment-link" >
-              A3 - JAVASCRIPT + REACT <AssignmentsControlButtons />
+              <BsGripVertical className="me-2 fs-3" /> <LuNotepadText className="me-2 fs-3"/> A3 - JAVASCRIPT + REACT <AssignmentsControlButtons />
             </a>
             <p> Multiple Modules | <b>Not Available </b> until May 20 at 12:00 am |
             <b> Due</b> May 27 at 11:59 pm | 100 pts </p>
-          </li>
-        </ul>
+          </ListGroup.Item >
+        </ListGroup>
       
      
     </ListGroup.Item>
