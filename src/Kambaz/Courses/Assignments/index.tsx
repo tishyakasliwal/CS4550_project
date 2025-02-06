48ad4ef7415715f3cc03086d7f19dcecd63948c4
Tishya Kasliwal <tishyakasliwal@gmail.com>
2025-02-05 23:56:36 -0500

    more edits

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -2,6 +2,7 @@ import { ListGroup } from "react-bootstrap";
 import AssignmentControls from "./AssignmentsControls";
 import { BsGripVertical } from "react-icons/bs";
 import AssignmentsBannerButtons from "./AssignmentsBannerButtons";
+import AssignmentsControlButtons from "./AssignmentsControlButtons";
 export default function Assignments() {
     return (
       <div id="wd-assignments">
@@ -18,14 +19,14 @@ export default function Assignments() {
           <li className="wd-assignment-list-item">
             <a href="#/Kambaz/Courses/1234/Assignments/123"
                className="wd-assignment-link" >
-              A1 - ENV + HTML
+              A1 - ENV + HTML <AssignmentsControlButtons />
             </a> 
             <p> Multiple Modules | <b>Not Available </b> until May 6 at 12:00 am |
             <b> Due</b> May 13 at 11:59 pm | 100 pts </p></li>
           <li className="wd-assignment-list-item">
             <a href="#/Kambaz/Courses/1234/Assignments/124"
                className="wd-assignment-link" >
-              A2 - CSS + BOOTSTRAP
+              A2 - CSS + BOOTSTRAP <AssignmentsControlButtons />
             </a> <p> Multiple Modules | <b>Not Available </b> until May 13 at 12:00 am |
             <b> Due</b> May 20 at 11:59 pm | 100 pts </p>
 
@@ -33,7 +34,7 @@ export default function Assignments() {
           <li className="wd-assignment-list-item">
             <a href="#/Kambaz/Courses/1234/Assignments/125"
                className="wd-assignment-link" >
-              A3 - JAVASCRIPT + REACT
+              A3 - JAVASCRIPT + REACT <AssignmentsControlButtons />
             </a>
             <p> Multiple Modules | <b>Not Available </b> until May 20 at 12:00 am |
             <b> Due</b> May 27 at 11:59 pm | 100 pts </p>
