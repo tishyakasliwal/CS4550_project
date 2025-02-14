e9160516f475eb813af822cc476858f1fd45dbb8
Tishya Kasliwal <tishyakasliwal@gmail.com>
2025-02-13 22:10:31 -0500

    3.5 and 3.6 done

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -27,8 +27,13 @@ import FunctionDestructing from "./FunctionDestructing";
 import DestructingImports from "./DestructingImports";
 import Classes from "./classes/Classes";
 import Styles from "./Styles";
+import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
+import PathParameters from "./PathParameters";
 
 export default function Lab3() {
+  console.log('Hello World!');
   return(
     <div id="wd-lab3">
       <h3>Lab 3</h3>
@@ -61,6 +66,18 @@ export default function Lab3() {
       <DestructingImports/>
       <Classes/>
       <Styles/>
+      
+      <Add a={3} b={4}/>
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+      </Highlight>
+      <PathParameters/>
+
+
 
     </div>
   );
